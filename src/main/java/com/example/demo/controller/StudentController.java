69de335fa47d632f4726f87dfd7e49b5eef691fd
bhoomisharma0908 <bhoomish5683@gmail.com>
2026-02-01 12:31:36 +0530

    bhoome changes 1

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -20,7 +20,7 @@ public class StudentController {
 
     @GetMapping("/all-students")
     public List<Student> getAllStrudents(){
-        System.out.println("My commit645");
+        System.out.println("My commit6455");
         return studentDetailsService.getStudents();
     }
 
