f4324d3bd2c1dc186ceb67873aca205d7d2ba89a
bhoomisharma0908 <bhoomish5683@gmail.com>
2026-02-21 13:32:06 +0530

    added POST API for student

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -3,12 +3,10 @@ package com.example.demo.controller;
 import com.example.demo.pojo.Student;
 import com.example.demo.service.StudentDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/stud")
@@ -23,10 +21,19 @@ public class StudentController {
         System.out.println("My commit645576");
         return studentDetailsService.getStudents();
     }
+    @GetMapping("/delete")
+    public String delete(@RequestParam int id){
+        return studentDetailsService.delete(id);
+    }
 
     @GetMapping("/student-by-id")
     public Student getStudentById(@RequestParam(name = "id", required = true) int id){
         return studentDetailsService.getStudentById(id);
     }
+    @PostMapping("/add-student")
+    public Student addStudent(@RequestBody Student student){
+        return studentDetailsService.addStudent(student);
+    }
+
 
 }
--- src/main/java/com/example/demo/service/StudentDetailsService.java
+++ src/main/java/com/example/demo/service/StudentDetailsService.java
@@ -3,22 +3,29 @@ package com.example.demo.service;
 import com.example.demo.pojo.Student;
 import com.example.demo.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
 
 @Service
 public class StudentDetailsService {
 
-    @Autowired
+    @Autowired //field injection
     private StudentRepository studentRepository;
 
-    public List<Student> getStudents(){
+    public List<Student> getStudents() {
         return studentRepository.findAll();
+    }
+    public String delete(int id){
+        studentRepository.deleteById(id);
+        return Integer.toString(id);
     }
 
 
     public Student getStudentById(int id) {
         return studentRepository.getReferenceById(id);
+    }
+    public Student addStudent(Student student){
+        return studentRepository.save(student);
     }
 }
